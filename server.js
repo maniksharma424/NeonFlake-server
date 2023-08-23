fca169376596af74b4610562632a09958915716d
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-08-23 16:35:00 +0530

    added CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import thumbnailRoutes from "./routes/thumbnailRoutes.js";
 import connectDB from "./config/db.js";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
@@ -9,14 +10,13 @@ connectDB();
 const port = process.env.PORT || 5000``;
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/thumbnails", thumbnailRoutes);
-
-
 
 app.get("/", (req, res) => {
   res.send("server is ready");
